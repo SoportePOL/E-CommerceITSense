e6c7bd8fd49986273a991d1e82a1bb14da5b29fd
Proveedores Online <Proveedores Online@LAPTOP-3L60AP5J>
2018-03-25 17:34:29 -0500

    Productos

---
+++ E-CommersITSense/E-Commerce.Model/Models/ECommerce/Products/Buying_modes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Model.Models.ECommerce.CategoriesProducts
+{
+    class Buying_modes
+    {
+        public string auction { get; set; }
+        public string buy_it_now { get; set; }
+        public string classified { get; set; }
+    }
+}
