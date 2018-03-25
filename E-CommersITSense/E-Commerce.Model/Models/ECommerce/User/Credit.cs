7bf42923f0d4223d6f3f2fb908f4fcce011dc2f6
julmartinez <juliand.martinezf@unilibrebog.edu.co>
2018-03-25 17:54:58 -0500

    CMB

---
+++ E-CommersITSense/E-Commerce.Model/Models/ECommerce/User/Credit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Model.Models.ECommerce
+{
+     public class Credit
+    {
+        public string consumed { get; set; }
+        public string credit_level_id { get; set; }
+    }
+}
---
+++ E-CommersITSense/E-Commerce.Model/Models/ECommerce/User/metrics.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_Commerce.Model.Models.ECommerce
+{
+    public class Metrics
+    {
+        public Sales sales { get; set; }
+        public Claims claims { get; set; }
+        public DelayedHandlingTime delayed_handling_time { get; set; }
+
+
+    }
+}
