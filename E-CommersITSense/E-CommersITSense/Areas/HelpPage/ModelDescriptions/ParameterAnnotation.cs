25406cb46394b8c8044fa758976e0a14c33510f4
Proveedores Online <Proveedores Online@LAPTOP-3L60AP5J>
2018-03-25 14:30:02 -0500

    Upload

---
+++ E-CommersITSense/E-CommersITSense/Areas/HelpPage/ModelDescriptions/ParameterAnnotation.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace E_CommersITSense.Areas.HelpPage.ModelDescriptions
+{
+    public class ParameterAnnotation
+    {
+        public Attribute AnnotationAttribute { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
---
+++ E-CommersITSense/E-CommersITSense/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace E_CommersITSense.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
